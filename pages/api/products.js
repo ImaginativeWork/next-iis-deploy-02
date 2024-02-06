5862c094390da4cc68a4e2380ef138c2ed7f1928
Doug Franklin <dfranklin@copt.com>
2024-02-06 17:36:03 -0500

    Same code

--- pages/api/products.js
+++ pages/api/products.js
@@ -4,9 +4,13 @@ import prisma from "../../lib/prisma";
 export default async function handle(req, res) {
 
   // const products = await prisma.product.findMany();
+  // const experimentalProducts = await prisma.products.findMany();
 
-  const experimentalProducts = await prisma.products.findMany();
+  // console.log('experimental products', experimentalProducts);
+
+  const pString = prisma;
 
   const products = await "Replace this with database items";
-  res.json(products);
+  // res.json(products);
+  res.json(prisma);
 }
